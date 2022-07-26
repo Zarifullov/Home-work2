abaac497427f6e267d922eb637c6536cc71495fd
Renat <rzarifullov@gmail.com>
2022-07-26 15:34:59 +0300

    Task3 added

---
+++ Task 3/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = 0;
+
+Console.WriteLine("Введите цифру обозначающую день недели");
+number = int.Parse(Console.ReadLine());
+
+if (number>=6 && number<=7)
+{
+    Console.WriteLine($"{number}-> Да");
+}
+else
+Console.WriteLine($"{number}-> Нет");
