b15218a62c24c82c3ccede69283b6a380d9e189f
Renat <rzarifullov@gmail.com>
2022-07-26 10:29:02 +0300

    Task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.WriteLine("Введите трёхзначное число");
+string x = Console.ReadLine();
+
+
+for(int i = 0; i < x.Length; i++)
+
+Console.WriteLine ();
+
+Console.WriteLine($"Второе число = {x [1]}");
+
