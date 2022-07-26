db938e044dd911f3848dea61663b708ae4ddb131
Renat <rzarifullov@gmail.com>
2022-07-26 15:11:07 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int x = 0;
+Console.WriteLine("Введите число");
+x = int.Parse(Console.ReadLine());
+
+if (x > 99)
+{
+   Console.WriteLine(x.ToString()[2]);
+}
+else 
+Console.WriteLine("Третьей цифры нет");
